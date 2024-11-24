2454ca35ff19f365a778cbe58ffb563c1ada5e6c
Alvaro U <aulian@ceibal.edu.uy>
2024-11-24 12:03:55 -0300

    [UPDATE]. actualizaciones varias

--- react-course/src/Components/ProductDetail/index.jsx
+++ react-course/src/Components/ProductDetail/index.jsx
@@ -9,7 +9,7 @@ const ProductDetail = () => {
   return (
     <aside
       className={`${context.isProductDetailOpen ? 'flex' : 'hidden'} product-detail flex-col fixed right-0 border border-black rounded-lg bg-white`}>
-      <div className='flex justify-between items-center p-6'>
+      {/* <div className='flex justify-between items-center p-6'>
         <h2 className='font-medium text-xl'>Detail</h2>
         <div>
           <XMarkIcon
@@ -27,7 +27,7 @@ const ProductDetail = () => {
         <span className='font-medium text-2xl mb-2'>${context.productToShow.price}</span>
         <span className='font-medium text-md'>${context.productToShow.title}</span>
         <span className='font-light text-sm'>${context.productToShow.description}</span>
-      </p>
+      </p> */}
     </aside>
   )
 }
